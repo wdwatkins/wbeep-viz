edafefd5f89dfa1fcd9cc3bdae4babbd6d93c77f
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-08-13 16:31:26 -0500

    removed unneeded div

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,36 @@
 <template>
   <div id="app">
-    <div class="main-container">
-      <HeaderUSWDSBanner />
-      <HeaderUSGS />
-      <HeaderUSWDSSelections title="Water Budget Estimation and Evaluation" />
-      <MapBox title="A Title Related to Water Availability" />
-    </div>
+    <HeaderUSWDSBanner />
+    <HeaderUSGS />
+    <HeaderUSWDSSelections title="Water Budget Estimation and Evaluation" />
+    <MapBox title="A Title Related to Water Availability" />
     <FooterUSGS />
   </div>
 </template>
 
 <script>
-  import HeaderUSWDSBanner from './components/HeaderUSWDSBanner'
-  import HeaderUSWDSSelections from './components/HeaderUSWDSSelections'
-  import HeaderUSGS from './components/HeaderUSGS'
-  import MapBox from './components/MapBox'
-  import FooterUSGS from './components/FooterUSGS'
+    import HeaderUSWDSBanner from './components/HeaderUSWDSBanner'
+    import HeaderUSWDSSelections from './components/HeaderUSWDSSelections'
+    import HeaderUSGS from './components/HeaderUSGS'
+    import MapBox from './components/MapBox'
+    import FooterUSGS from './components/FooterUSGS'
 
-export default {
-  name: 'App',
-  components: {
-    HeaderUSWDSBanner,
-    HeaderUSGS,
-    HeaderUSWDSSelections,
-    MapBox,
-    FooterUSGS
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HeaderUSWDSBanner,
+            HeaderUSGS,
+            HeaderUSWDSSelections,
+            MapBox,
+            FooterUSGS
+        }
+    }
 </script>
 
 <style lang="scss">
-  * {
-    box-sizing: border-box;
-  }
-  *:before,
-  *:after {
-    box-sizing: border-box;
-  }
-  html,
-  body {
+  body{
     margin: 0;
     padding: 0;
-    height: 100%;
-    position: relative;
   }
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -50,12 +38,4 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
   }
-  .main-container {
-    min-height: 100vh; /* will cover the 100% of viewport */
-    overflow: hidden;
-    display: block;
-    position: relative;
-    padding-bottom: 100px; /* height of footer */
-  }
-
 </style>
