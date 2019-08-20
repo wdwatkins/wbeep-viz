5de4ad3fc5e21174fb2bf280af8e72680b5f8822
Chip Orr <tworr@usgs.gov>
2019-08-20 15:21:21 -0700

    Rework automated build files to allow for both an HRU and base tile source, and to allow for manual specification of tile source location. Reset src/components/MapBox.vue to match master branch since the tile location configuration has been moved to another file.

--- config.sh
+++ config.sh
@@ -1,25 +1,52 @@
 #!/usr/bin/env sh
 
-# Mapbox configuration file location
-mbconfig=src/components/MapBox.vue
+# This script inserts the corect HRU and basemap tile
+# locations into the appropriate Mapbox configuration file
+# before an automated build of the vue app.
+#
+# Two sets of tiles are available, one in the test bucket
+# and one in the prod bucket.  By default the app
+# in the test bucket will use the test tiles, while the qa, beta
+# and prod builds will all use tiles from prod.
+#
+# There will be an option in Jenkins to override the tile 
+# locations so that for example we can do a beta build that
+# uses tiles from test.
 
-# bucket locations
-prodbucket=wbeep-prod-website.s3-website-us-west-2.amazonaws.com
-testbucket=wbeep-test-website.s3-website-us-west-2.amazonaws.com
-qabucket=wbeep-qa-website.s3-website-us-west-2.amazonaws.com
-betabucket=wbeep-beta-website.s3-website-us-west-2.amazonaws.com
+# Mapbox configuration file where tile locations are defined
+mbconfig=src/assets/mapStyles/mapStyles.js
 
-# common prefix/suffix for Mapbox tile URL configuration
-hruprefix='"tiles": ["http://'
-hrusuffix='/tiles/{z}/{x}/{y}.pbf"],'
+# S3 bucket addresses where tiles are stored
+# note: once we have a public URL for prod we'll need
+# to change that here
+prodBucket=wbeep-prod-website.s3-website-us-west-2.amazonaws.com
+testBucket=wbeep-test-website.s3-website-us-west-2.amazonaws.com
 
-# Set up the Mapbox tile URL (use beta if no target given)
-case $E_BUILDTARGET in
-  "prod") hrusource="$hruprefix$prodbucket$hrusuffix";;
-  "test") hrusource="$hruprefix$testbucket$hrusuffix";;
-  "qa") hrusource="$hruprefix$qabucket$hrusuffix";;
-  *) hrusource="$hruprefix$betabucket$hrusuffix";;
-esac
+# common prefix/suffix for tile URL configuration
+globalTilePrefix='"tiles": ["https://'
+globalTileSuffix='/{z}/{x}/{y}.pbf"],'
+# tile directories
+hruTileLocation='/tiles'
+baseTileLocation='/basetiles'
 
-# Insert Mapbox tile URL into Mapbox configuration file
-sed -i "/HRU SOURCE INSERT/a $hrusource" "$mbconfig"
+# Since most build cases will use tiles from prod, we'll set
+# that as the default tile source location
+hruSource="$globalTilePrefix$prodBucket$hruTileLocation$globalTileSuffix"
+baseSource="$globalTilePrefix$prodBucket$baseTileLocation$globalTileSuffix"
+
+# Most builds will use tiles from prod.  The only exceptions are if
+# the tile source (passed in $E_TILESOURCE) is manually set to "test",
+# or if we are bulding to test.  Note that if $E_TILESOURCE is not
+# present or is "default" then we use default tile locations.
+if [(($E_TILESOURCE == 'default' || -z "$E_TILESOURCE") && $E_BUILDTARGET == 'test') || ($E_TILESOURCE == 'test') ]
+then
+  hruSource="$globalTilePrefix$testBucket$hruTileLocation$globalTileSuffix"
+  baseSource="$globalTilePrefix$testBucket$baseTileLocation$globalTileSuffix"   
+else
+  hruSource="$globalTilePrefix$prodBucket$hruTileLocation$globalTileSuffix"
+  baseSource="$globalTilePrefix$prodBucket$baseTileLocation$globalTileSuffix" 
+fi
+
+# Insert HRU and base tile URLs into Mapbox configuration file
+sed -i "/HRU SOURCE INSERT/a $hruSource" "$mbconfig"
+sed -i "/BASE SOURCE INSERT/a $baseSource" "$mbconfig"
