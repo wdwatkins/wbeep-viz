162d063326f7c3c5d2c4de85799b25b3f41131d3
Wernimont <mwernimont@igsarmewltmrwm.gs.doi.net>
2019-09-19 11:33:15 -0500

    Add Subtitle information modal

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import uswds from 'uswds'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLayerGroup } from '@fortawesome/free-solid-svg-icons'
 import { faInfo } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 library.add(faLayerGroup)
 library.add(faInfo)
+library.add(faTimes)
 
 Vue.config.productionTip = false
 Vue.use(uswds)
